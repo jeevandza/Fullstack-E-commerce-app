b161ddc83de98043f7710cad4becf8c112c209f5
jeevandza <jeevandza4@gmail.com>
2024-02-28 14:01:37 +0530

    added error module

--- server/src/utils/config/hpptError.js
+++ server/src/utils/config/hpptError.js
@@ -1,6 +1,35 @@
-const errorTypes={
-
-}
 
 
-module.export = errorTypes
+const httpStatusCodes = {
+    OK: 200,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER: 500
+   }
+   
+class BaseError extends Error {
+    constructor (name, statusCode, isOperational, description) {
+    super(description)
+   
+    Object.setPrototypeOf(this, new.target.prototype)
+    this.name = name
+    this.statusCode = statusCode
+    this.isOperational = isOperational
+    Error.captureStackTrace(this)
+    }
+   }
+
+
+   class Api404Error extends BaseError {
+    constructor (
+    name,
+    statusCode = httpStatusCodes.NOT_FOUND,
+    description = 'Not found.',
+    isOperational = true
+    ) {
+    super(name, statusCode, isOperational, description)
+    }
+   }
+   
+   module.exports = BaseError
+
