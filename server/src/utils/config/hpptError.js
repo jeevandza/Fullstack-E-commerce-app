2850380c2fae5582a9c6054b5e5be22fecea0317
jeevandza <jeevandza4@gmail.com>
2024-03-01 23:18:05 +0530

    added https errors

--- server/src/utils/config/hpptError.js
+++ server/src/utils/config/hpptError.js
@@ -1,35 +1,53 @@
-
-
 const httpStatusCodes = {
-    OK: 200,
-    BAD_REQUEST: 400,
-    NOT_FOUND: 404,
-    INTERNAL_SERVER: 500
-   }
-   
+  OK: 200,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER: 500,
+};
+
 class BaseError extends Error {
-    constructor (name, statusCode, isOperational, description) {
-    super(description)
-   
-    Object.setPrototypeOf(this, new.target.prototype)
-    this.name = name
-    this.statusCode = statusCode
-    this.isOperational = isOperational
-    Error.captureStackTrace(this)
-    }
-   }
+  constructor(name, statusCode, isOperational, description) {
+    super(description);
 
+    Object.setPrototypeOf(this, new.target.prototype);
+    this.name = name;
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+    Error.captureStackTrace(this);
+  }
+}
 
-   class Api404Error extends BaseError {
-    constructor (
+class Api404Error extends BaseError {
+  constructor(
     name,
     statusCode = httpStatusCodes.NOT_FOUND,
-    description = 'Not found.',
+    description = "Not found.",
     isOperational = true
-    ) {
-    super(name, statusCode, isOperational, description)
-    }
-   }
-   
-   module.exports = BaseError
+  ) {
+    super(name, statusCode, isOperational, description);
+  }
+}
 
+class Api500Error extends BaseError {
+  constructor(
+    name,
+    statusCode = httpStatusCodes.INTERNAL_SERVER,
+    description = "Internal server error",
+    isOperational = false
+  ) {
+    super(name, statusCode, description, isOperational);
+  }
+}
+
+class Api400Error extends BaseError {
+  constructor(
+    name,
+    statusCode = httpStatusCodes.NOT_FOUND,
+    description = "Bad request",
+    isOperational = true
+  ) {
+    super(name, statusCode, description, isOperational);
+  }
+}
+
+module.exports = { Api404Error, Api500Error, Api400Error };
