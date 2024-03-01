61fee14d7bc5c111b50dbc79abfa718ce35e3050
jeevandza <jeevandza4@gmail.com>
2024-03-01 10:23:23 +0530

    added xlsx package to handle xl files

--- server/src/routes/csvDownload.js
+++ server/src/routes/csvDownload.js
@@ -1,23 +1,28 @@
 const { Router } = require("express");
 const User = require("../models/User.model");
 const papParse = require("papaparse")
+const xlsx = require('xlsx');
+
 
 const downloadCsvRoute = Router();
 
 downloadCsvRoute.get("", async (req, res) => {
-  res.setHeader("Content-Type", "text/csv");
-  res.setHeader("Content-Disposition", 'attachment; filename="users.csv"');
+  try {
+    const userList = await User.find();
+    const ws = xlsx.utils.json_to_sheet(userList);
+    const wb = xlsx.utils.book_new();
+    xlsx.utils.book_append_sheet(wb, ws, 'Users');
 
+    const buffer = xlsx.write(wb, { type: 'buffer', bookType: 'xlsx' });
 
-  const userList = await User.find();
-  const csvString = papParse.unparse(userList);
+    res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+    res.setHeader("Content-Disposition", 'attachment; filename="users.xlsx"');
 
-  
-  const blob = new Blob([csvString], { type: 'text/csv' });
-
-
- return res.send(blob);
+    res.send(buffer);
+  } catch (error) {
+    console.error("Error downloading XLSX file:", error);
+    res.status(500).send("Error downloading XLSX file");
+  }
 });
-
 
 module.exports = downloadCsvRoute
